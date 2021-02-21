ee1cb25c3a5c3f42aa549ab7eb70b1e330c763ef
Ryosuke <hello.ryosuke.u@gmail.com>
2021-02-21 13:55:05 +0900

    Add test

--- src/test/java/com/b1a9idps/resource/GreetingResourceTest.java
+++ src/test/java/com/b1a9idps/resource/GreetingResourceTest.java
@@ -7,15 +7,25 @@ import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
 @QuarkusTest
-public class GreetingResourceTest {
+class GreetingResourceTest {
 
     @Test
-    public void testHelloEndpoint() {
+    void hello() {
         given()
                 .when().get("/hello")
                 .then()
                 .statusCode(200)
                 .body(is("Hello RESTEasy"));
+    }
+
+    @Test
+    void greeting() {
+        given()
+                .pathParam("name", "uchitate")
+                .when().get("/hello/greeting/{name}")
+                .then()
+                .statusCode(200)
+                .body(is("hello uchitate"));
     }
 
 }
