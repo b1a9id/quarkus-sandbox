a4bf9c8eb5165db0294d7ebc374ece7e635dac0a
Ryosuke <hello.ryosuke.u@gmail.com>
2021-02-21 14:18:02 +0900

    Fix

--- src/main/java/com/b1a9idps/service/impl/GreetingServiceImpl.java
+++ src/main/java/com/b1a9idps/service/impl/GreetingServiceImpl.java
@@ -7,6 +7,7 @@ import javax.enterprise.context.ApplicationScoped;
 @ApplicationScoped
 public class GreetingServiceImpl implements GreetingService {
 
+    @Override
     public String greeting(String name) {
         return "hello " + name;
     }
